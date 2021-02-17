98f31be291366bae4e3238946cafc6ac8f264b87
Sujeet Kumar <sujeetk@fractalanalytics.com>
2021-02-17 07:03:51 +0530

    Initial Commit

---
+++ UIKitToSwiftUI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitToSwiftUI
+//
+//  Created by Sujeet.Kumar on 17/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
